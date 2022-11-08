7caad7f8cbe8f95a8b62fa9d08ad0c016892df34
Freddie Coleman <f.coleman@hotmail.co.uk>
2022-11-08 16:22:11 +0000

    bootstrap connection to DataLayer

--- src/tail.database.module.ts
+++ src/tail.database.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { connectionOptions } from './config/rpc.config';
+import { DataLayer } from './tail-database-client/datalayer/rpc/data_layer';
 import { Tail } from './tail-database-client/models/tail/model';
+
+const datalayer = new DataLayer({
+    ...connectionOptions,
+    // Temporarily hacking in testnet10 store id
+    id: '073edb36a4a982c3d00999b1d925d304e7867afa68eb535e3071ee2f682700ea',
+})
+const tail = new Tail(datalayer);
 
 @Module({
   imports: [],
   controllers: [],
-  providers: [Tail],
+  providers: [{
+    provide: Tail,
+    useValue: tail
+}],
   exports: [Tail],
 })
 export class TailDatabaseModule {}
